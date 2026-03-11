4cce1d1046f74265a452bf03a916f106e36fd932
wackydawg 🤪 <juliannwadinobi098@gmail.com>
2026-03-11 22:02:32 +0100

    chore: start semgrep scan on plugins

---
+++ plugins/wp-sms-vatansms-com/includes/admin/welcome/welcome.php
@@ -0,0 +1,6 @@
+<div class="wrap wps-wrap about-wrap full-width-layout">
+    
+        <div class="wps-return-to-dashboard">
+            <a href="admin.php?page=wp-sms-settings"><?php _e( 'VatanSMS WP-SMS &rarr; Ayarlari', 'wp-sms' ); ?></a>
+        </div>
+</div>
---
+++ plugins/wp-sms-vatansms-com/vatansms-wp-sms.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: WP SMS VatanSMS.com
+ * Plugin URI: https://vatansms.com/
+ * Description: VatanSMS WordPress
+ * Version: 1.01
+ * Author: VatanSMS
+ * Author URI: https://vatansms.com/
+ * Text Domain: WP SMS VatanSMS.com
+ * Domain Path: https://vatansms.com/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+/**
+ * Load Plugin Defines
+ */
+require_once 'includes/defines.php';
+
+/**
+ * Load plugin Special Functions
+ */
+require_once 'includes/functions.php';
+
+/**
+ * Get plugin options
+ */
+$wpsms_option = get_option( 'wpsms_settings' );
+
+/**
+ * Initial gateway
+ */
+require_once 'includes/class-wpsms-gateway.php';
+
+$sms = wp_sms_initial_gateway();
+
+/**
+ * Load Plugin
+ */
+require 'includes/class-wpsms.php';
+
+new WP_SMS();
