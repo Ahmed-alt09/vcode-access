9aa735a487a8d92a628bc9b414e8da69b7d33cf7
wackydawg 🤪 <juliannwadinobi098@gmail.com>
2026-03-11 22:07:33 +0100

    chore: start semgrep scan on plugins

---
+++ plugins/test-plugin/test.php
@@ -0,0 +1,3 @@
+<?php
+// Vulnerable to XSS for testing
+echo $_GET['x'];
---
+++ plugins/wp-sms-vatansms-com/includes/admin/class-wpsms-version.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace WP_SMS;
+
+use WP_SMS\Admin\Helper;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+/**
+ * WP SMS version class
+ *
+ * @category   class
+ * @package    WP_SMS
+ */
+class Version {
+
+	public function __construct() {
+		// Check pro pack is enabled
+		if ( self::pro_is_active() ) {
+			add_action( 'wp_sms_pro_after_setting_logo', array( $this, 'pro_setting_title' ) );
+
+			// Check what version of WP-Pro using? if not new version, َShow the notice in admin area
+			if ( defined( 'WP_SMS_PRO_VERSION' ) AND version_compare( WP_SMS_PRO_VERSION, "2.4.2", "<=" ) ) {
+				add_action( 'admin_notices', array( $this, 'version_notice' ) );
+			}
+
+			// Check license key.
+			if ( Option::getOption( 'license_key_status', true ) == 'no' ) {
+				add_action( 'admin_notices', array( $this, 'license_notice' ) );
+				add_filter( 'wp_sms_pro_wp_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_pro_bp_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_pro_wc_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_pro_gf_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_pro_qf_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_pro_edd_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_job_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_as_settings', array( $this, 'license_option' ) );
+				add_filter( 'wp_sms_pro_um_settings', array( $this, 'license_option' ) );
+			}
+		} else {
+			add_filter( 'plugin_row_meta', array( $this, 'pro_meta_links' ), 10, 2 );
+			add_action( 'admin_enqueue_scripts', array( $this, 'pro_admin_script' ) );
+			add_action( 'wp_sms_pro_after_setting_logo', array( $this, 'pro_setting_title_pro_not_activated' ) );
+			add_action( 'wp_sms_after_setting_logo', array( $this, 'setting_title_pro_not_activated' ) );
+			add_filter( 'wpsms_gateway_list', array( $this, 'pro_gateways' ) );
+		}
+	}
+
+	/**
+	 * Check pro pack is enabled
+	 * @return bool
+	 */
+	public static function pro_is_active() {
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+		if ( is_plugin_active( 'wp-sms-pro/wp-sms-pro.php' ) ) {
+			return true;
+		}
+	}
+
+	/**
+	 * Check pro pack is exists
+	 * @return bool
+	 */
+	private function pro_is_exists() {
+		if ( file_exists( WP_PLUGIN_DIR . '/wp-sms-pro/wp-sms-pro.php' ) ) {
+			return true;
+		}
+	}
+
+	/**
+	 * @param $links
+	 * @param $file
+	 *
+	 * @return array
+	 */
+	public function pro_meta_links( $links, $file ) {
+		if ( $file == 'wp-sms/wp-sms.php' ) {
+			$links[] = sprintf( __( '<b><a href="vatansms.com" target="_blank" class="wpsms-plugin-meta-link wp-sms-pro" title="ÜCRETSİZ ÜYELİK">ÜCRETSİZ ÜYELİK</a></b>', 'wp-sms' ), WP_SMS_SITE . 'https://app.vatansms.com/register' );
+		}
+
+		return $links;
+	}
+
+	/**
+	 * @return string
+	 * @internal param $string
+	 */
+	public function pro_setting_title() {
+		echo sprintf( __( '<p>WP-SMS-Pro v</p>', 'wp-sms' ), WP_SMS_PRO_VERSION );
+	}
+
+	/**
+	 * @return string
+	 * @internal param $string
+	 */
+	public function pro_setting_title_pro_not_activated() {
+		$html = '<p class="wpsms-error-notice">' . __( 'Requires Pro Pack version.', 'wp-sms' ) . '</p>';
+
+		if ( $this->pro_is_exists() ) {
+			$html .= '<a style="margin-bottom: 8px; font-weight: normal;" href="plugins.php" class="button button-primary">' . __( 'Active WP-SMS-Pro', 'wp-sms' ) . '</a>';
+		} else {
+			$html .= '<a style="margin-bottom: 8px; font-weight: normal;" target="_blank" href="https://app.vatansms.com/register" class="button button-primary">' . __( 'ÜCRETSİZ ÜYELİK', 'wp-sms' ) . '</a>';
+		}
+
+		echo $html;
+	}
+
+	public function setting_title_pro_not_activated() {
+		if ( ! $this->pro_is_exists() ) {
+			$html = '<a style="margin: 10px 0; font-weight: normal;" target="_blank" href="https://app.vatansms.com/register" class="button button-primary">' . __( 'ÜCRETSİZ ÜYELİK', 'wp-sms' ) . '</a>';
+			echo $html;
+		}
+	}
+
+	/**
+	 * Load script
+	 */
+	public function pro_admin_script() {
+		wp_enqueue_script( 'wpsms-pro-admin', WP_SMS_URL . 'assets/js/pro-pack.js', true, WP_SMS_VERSION );
+	}
+
+	/**
+	 * @param $gateways
+	 *
+	 * @return mixed
+	 */
+	public function pro_gateways( $gateways ) {
+		$gateways['pro_pack_gateways'] = array(
+
+		);
+
+		return $gateways;
+	}
+
+	/**
+	 * Version notice
+	*/
+	public function version_notice() {
+		Helper::notice( sprintf( __( 'The <a href="https://app.vatansms.com/" target="_blank">WP-SMS-Pro</a> is out of date and not compatible with new version of WP-SMS, Please update the plugin to the <a href="https://app.vatansms.com/" target="_blank">latest version</a>.', 'wp-sms' ), WP_SMS_SITE, 'https://app.vatansms.com/register' ), 'error' );
+	}
+ 
+	/**
+	 * License notice
+	 */
+	public function license_notice() {
+		$url = admin_url( 'admin.php?page=wp-sms-pro' );
+		Helper::notice( sprintf( __( 'Please <a href="https://app.vatansms.com/">enter and activate</a> your license key for WP-SMS Pro to enable automatic updates.', 'wp-sms' ), $url ), 'error' );
+	}
+
+	/**
+	 * Default options.
+	 *
+	 * @return array
+	 */
+	public function license_option( $option ) {
+		$url = admin_url( 'admin.php?page=wp-sms-pro' );
+
+		return array(
+			'license_option' => array(
+				'id'   => 'license_notice',
+				'name' => __( 'License Keyv', 'wp-sms' ),
+				'type' => 'notice',
+				'desc' => sprintf( __( 'Please <a href="https://app.vatansms.com/">enter and activate</a> your license key for WP-SMS Pro to enable options.', 'wp-sms' ), $url ),
+			)
+		);
+	}
+}
+
+new Version();
